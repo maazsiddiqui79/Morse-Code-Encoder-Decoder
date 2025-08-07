7a22528958f406c3784ffca69e130cfdfc0ddcc4
maazsiddiqui79 <siddiqui.maaz79@gmail.com>
2025-08-07 22:31:55 +0530

    Update Fixed

--- api/app.py
+++ api/app.py
@@ -1,7 +1,39 @@
 # api/app.py
 
 from flask import render_template, redirect, url_for, Flask, request
-from encdoer_and_decoder import ENC_AND_DEC
+
+
+
+class ENC_AND_DEC:
+    lst = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 
+    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "|", ":", "\"", "<", ">", "?", "~", "-", " "
+]
+
+    def encrypt(self,og_txt, shift):
+        cipher_text = ""
+        
+        for i in og_txt:
+                shifted_position = self.lst.index(i) + int(shift)
+                shifted_position %= len(self.lst)
+                cipher_text += self.lst[shifted_position]
+        
+        return(cipher_text)
+
+    def decrypt(self,og_txt, shift):
+        cipher_text = ""
+        
+        for i in og_txt:
+            
+            
+                shifted_position = self.lst.index(i) - int(shift)
+                shifted_position %= len(self.lst)
+                cipher_text += self.lst[shifted_position]
+        
+        return(cipher_text)
+        
 
 app = Flask(__name__)
 cipher = ENC_AND_DEC()
